56879c396d6825cfa600b414b119574bdde5264f
sachik1 <sachikelkar@gmail.com>
2025-11-14 13:25:44 -0800

    Initial React + Tailwind setup (CRA)

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        React + Tailwind (no Vite) 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
